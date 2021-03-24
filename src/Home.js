12bb9c77d48280666fe40d7932e9a8093a950307
Kajol Sharma <75063748+kajol-rep@users.noreply.github.com>
2021-03-25 00:15:23 +0530

    updated download links

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
     <div>
       <div className="horizontal-card">
         <img
-          style={{ width: "50%", height: "30rem" }}
+          style={{ width: "50%", height: "100%" }}
           alt=""
           src="https://uploads-ssl.webflow.com/5e6fd8b314a08bebab35c81a/5ff6a74fce7e0114a17adc81_undraw_Books_l33t.svg"
         />
@@ -18,6 +18,22 @@ export default function Home() {
           </p>
         </div>
       </div>
+      <p className="medium-text">
+        Before getting started include this link in your code to access the
+        styles.
+      </p>
+      <iframe
+        title="style"
+        src="https://carbon.now.sh/embed?bg=rgba%28191%2C220%2C238%2C0.22%29&t=vscode&wt=none&l=jsx&ds=false&dsyoff=20px&dsblur=73px&wc=true&wa=true&pv=0px&ph=0px&ln=false&fl=1&fm=dm&fs=15px&lh=133%25&si=false&es=4x&wm=false&code=%2520%2520%2520%2520%253Clink%2520rel%253D%2522stylesheet%2522%2520href%253D%2522https%253A%252F%252Fwrap-it.netlify.app%252FcomponentStyle.css%2522%253E%250A"
+        style={{
+          width: "706px",
+          height: "112px",
+          border: "0",
+          transform: "scale(1)",
+          overflow: "hidden"
+        }}
+        sandbox="allow-scripts allow-same-origin"
+      ></iframe>
     </div>
   );
 }
